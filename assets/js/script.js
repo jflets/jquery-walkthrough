ebadd9134531db60db380d45813fd74f1d488354
jflets <jfletorides02@gmail.com>
2023-08-17 08:44:46 +0100

    Add further functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
-$("button").mouseenter(function() {
-    $(this).removeClass("make-red").addClass("make-blue");
-});
-
-$("button").mouseleave(function() {
-    $(this).removeClass("make-blue").addClass("make-red");
-});
+ $("document").ready(function() {
+    $(".box").on("click", function() {
+        var classNames = $(this).attr("class").split(" ");
+        var boxClass = classNames[0];
+        var className = classNames[1]
+        if ($(this).css("background-color") == "rgba(255, 0, 0)") {
+            $("." + className).css("background-color", "#000");
+        }
+        else {
+            $("." + boxClass).css("background-color", "#000");
+            $("." + className).css("background-color", "red");
+        }
+    });
+ });
